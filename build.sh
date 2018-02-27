efb1541628ae4a430270e50f435b8d2419b4ef8a
Lucas Lara <lucaslorentzlara@hotmail.com>
2018-02-27 10:11:25 +0100

    Update packages

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 
 glide install
 
-sed -i '464i\\t"run",    // github.com/lucaslorentz/caddy-run' \
+sed -i '491i\\t"run",    // github.com/lucaslorentz/caddy-run' \
  vendor/github.com/mholt/caddy/caddyhttp/httpserver/plugin.go
 
 go build -o caddy

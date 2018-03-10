5df7ebc1813a281a5c5680368aa10c81dfd86061
Lucas Lara <lucaslorentzlara@hotmail.com>
2018-03-10 17:12:34 +0100

    Add Travis CI build

--- build.sh
+++ build.sh
@@ -4,7 +4,10 @@ set -e
 
 glide install
 
+go vet $(glide novendor)
+go test -race -v $(glide novendor)
+
 sed -i '491i\\t"run",    // github.com/lucaslorentz/caddy-run' \
  vendor/github.com/mholt/caddy/caddyhttp/httpserver/plugin.go
 
-go build -o caddy
+CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -o caddy

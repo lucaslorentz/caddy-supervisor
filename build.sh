1a43f61a2a47329b916837f46194482d48b8d61b
Lucas Lara <lucaslorentzlara@hotmail.com>
2019-01-26 16:22:31 +0100

    Migrate to dep

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-glide install
+dep ensure
 
-go vet $(glide novendor)
-go test -race -v $(glide novendor)
+go vet $(go list ./... | grep -v vendor)
+go test -race -v $(go list ./... | grep -v vendor)
 
 CGO_ENABLED=0 go build -o caddy
 

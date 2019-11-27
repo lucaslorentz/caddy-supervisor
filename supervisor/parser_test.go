506dd60aa90ba8a3544cd665da7e8ac254e37bde
Jayson Reis <santosdosreis@gmail.com>
2019-11-27 11:51:55 +0100

    Check also variable interpolation

--- supervisor/parser_test.go
+++ supervisor/parser_test.go
@@ -17,7 +17,7 @@ func TestParseOption(t *testing.T) {
 		expectedParseOptionReturn bool
 		assertOptions             func(*Options) error
 	}{
-		{"env with variable", args{caddy.NewTestController("http", "supervisor {\nenv NUMBER={{ add 1 .Replica }}\n}"), new(Options)}, true, func(options *Options) error {
+		{"env with variable", args{caddy.NewTestController("http", "supervisor {\nenv NUMBER={{ add 1 .Replica }}\n}"), CreateOptions()}, true, func(options *Options) error {
 			if len(options.Env) != 1 {
 				return errors.Errorf("Env should be exactly one and returned %v", len(options.Env))
 			}
@@ -28,13 +28,22 @@ func TestParseOption(t *testing.T) {
 				return errors.Errorf("env should be %v but returned %v", expected, returned)
 			}
 
+			supervisors := CreateSupervisors(options)
+			expected = "NUMBER=1"
+			returned = supervisors[0].options.Env[0]
+			if expected != returned {
+				return errors.Errorf("env should be %v but returned %v", expected, returned)
+			}
+
 			return nil
 		}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			tt.args.c.RemainingArgs()
+			processedBlocks := 0
 			for tt.args.c.NextBlock() {
+				processedBlocks += 1
 				if parseOptionReturn := ParseOption(tt.args.c, tt.args.options); parseOptionReturn != tt.expectedParseOptionReturn {
 					t.Errorf("ParseOption should return %v and returned %v", tt.expectedParseOptionReturn, parseOptionReturn)
 				}
@@ -42,6 +51,10 @@ func TestParseOption(t *testing.T) {
 				if err := tt.assertOptions(tt.args.options); err != nil {
 					t.Error(err)
 				}
+			}
+
+			if processedBlocks == 0 {
+				t.Errorf("this test case seems to be empty as no block was processed")
 			}
 		})
 	}
